9df2d562f483e5a886a13fb601a66613d36bb790
Angeld55 <achi_d@abv.bg>
2019-09-18 00:59:02 +0300

    Update LinkedStack.hpp

--- Stack/LinkedStack.hpp
+++ Stack/LinkedStack.hpp
@@ -1,1 +1,128 @@
-d
+#include <iostream>
+
+template <typename T>
+class LinkedStack
+{
+	struct Node
+	{
+		Node(T data)
+		{
+			this->data = data;
+			next = nullptr;
+		}
+		T data;
+		Node* next;
+	};
+	Node* head;
+
+	void Free();
+	void CopyFrom(const LinkedStack& other);
+public:
+	LinkedStack();
+	LinkedStack(const LinkedStack& other);
+	LinkedStack operator=(const LinkedStack& other);
+	~LinkedStack();
+
+
+	void Push(T);  //O(1)
+	T Pop(); //O(1)
+
+	bool isEmpty();
+
+};
+
+template <typename T>
+void LinkedStack<T>::Free()
+{
+	Node* iter = head;
+	while (iter != nullptr)
+	{
+		Node* prev = iter;
+		iter = iter->next;
+		delete prev;
+	}
+}
+
+template <typename T>
+void LinkedStack<T>::CopyFrom(const LinkedStack& other)
+{
+	Node* iter = other.head;
+	while (iter != nullptr)
+	{
+		AddLast(iter->data);
+		iter = iter->next;
+	}
+
+}
+
+template <typename T>
+LinkedStack<T>::LinkedStack()
+{
+	head = nullptr;
+}
+
+template <typename T>
+LinkedStack<T>::LinkedStack(const LinkedStack& other)
+{
+	CopyFrom(other);
+}
+
+template <typename T>
+LinkedStack<T> LinkedStack<T>::operator=(const LinkedStack& other)
+{
+	if (this != &other)
+	{
+		Free();
+		CopyFrom(other);
+	}
+	return *this;
+}
+
+
+template <typename T>
+void LinkedStack<T>::Push(T el)
+{
+	Node* newNode = new Node(el);
+	if (head == nullptr)//if its empty
+		head = newNode;
+	else
+	{
+		newNode->next = head;
+		head = newNode;
+	}
+}
+
+template <typename T>
+T LinkedStack<T>::Pop()
+{
+	if (isEmpty())
+		throw "The LinkedStack is empty";
+	else if (head->next==nullptr)
+	{
+		T el = head->data;
+		delete head;
+		head = nullptr;
+		return el;
+	}
+	else
+	{
+		T el = head->data;
+		Node* temp = head->next;
+		delete head;
+		head = temp;
+		return el;
+	}
+
+}
+
+template <typename T>
+bool LinkedStack<T>::isEmpty()
+{
+	return head == nullptr;
+}
+
+template <typename T>
+LinkedStack<T>::~LinkedStack()
+{
+	Free();
+}
